4e5f0b8e3da689bfc15e1125012f983b9c60ecd1
Rafael Impas <impasrafael@gmail.com>
2025-04-27 22:49:11 +0800

    Added vercel analytics function to layout file.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 	import '../app.css';
 	import Navbar from '../lib/components/Navbar.svelte';
 	let { children } = $props();
+	injectAnalytics();
 </script>
 
 <svelte:head>
-  <title>Rafael Impas - Developer</title>
-  <meta name="description" content="Web developer specializing in modern, responsive websites." />
-  <meta name="keywords" content="web development, SvelteKit, JavaScript" />
-  <meta property="og:title" content="Rafael Impas - Developer" />
-  <meta property="og:url" content="https://rafael-impas.vercel.app/" />
-  <meta property="og:image" content="https://rafael-impas.vercel.app/preview-image.png" />
-  <meta name="google-site-verification" content="wPgVmSFbbnOXcmLoh9ZvPga0Va2HPdodUP9gFUu_zkY" />
+	<title>Rafael Impas - Developer</title>
+	<meta name="description" content="Web developer specializing in modern, responsive websites." />
+	<meta name="keywords" content="web development, SvelteKit, JavaScript" />
+	<meta property="og:title" content="Rafael Impas - Developer" />
+	<meta property="og:url" content="https://rafael-impas.vercel.app/" />
+	<meta property="og:image" content="https://rafael-impas.vercel.app/preview-image.png" />
+	<meta name="google-site-verification" content="wPgVmSFbbnOXcmLoh9ZvPga0Va2HPdodUP9gFUu_zkY" />
 </svelte:head>
 
 <div class="h-full">
